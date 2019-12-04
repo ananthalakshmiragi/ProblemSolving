b812798b13d4b3941c6e03e04b4c27549679dff0
ananthalakshmiragi <anantha.lakshmi.ragi@gmail.com>
2019-12-04 11:07:35 +0530

    Update NumOfRotations.java

--- NumOfRotations.java
+++ NumOfRotations.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class NumOfRotations{
   public static void main(String []args){
-        List<Integer> arrInpList = Arrays.asList(7,8,1,2,3,4,5,6);
+        List<Integer> arrInpList = Arrays.asList(7,7,8,1,2,2,3,4,4,5,6);
         ArrayList<Integer> arrInp = new ArrayList<Integer>();
         arrInp.addAll(arrInpList);
         
@@ -10,17 +10,17 @@ public class NumOfRotations{
      }
      
     public static int fnNoOfRot(ArrayList<Integer> arrInp) {
-         int first=0, mid=0, last=arrInp.size()-1, i=0;
-         mid = first+last/2;
+         int first=0, mid=0, last=arrInp.size()-1;
+         mid = (first+last)/2;
          
-         while (first < last && first != mid && last != mid) {
+         while (first != mid && last != mid) {
              if (arrInp.get(first) <= arrInp.get(mid)) {
                  first = mid;
              }
              else {
                  last = mid;
              }
-             mid = first+last/2;
+             mid = (first+last)/2;
          }
          return last;
      }
